4d18a98a5af9a9d1d6b07b70f17fd2327cd4cfce
bastianlang@t-online.de <bastianlang@t-online.de>
2015-11-11 19:07:45 +0100

    fixed wrong indexing

--- assignment6/mlp.py
+++ assignment6/mlp.py
@@ -16,47 +16,46 @@ class MLP:
     
     def __init__(self, number_input, number_hidden, number_output, initial_weight_value):
         self.number_of_inputs = number_input
-        self.create_layer(self.hidden_layer, number_hidden)
-        self.create_layer(self.output_layer, number_output)
+        self.hidden_layer = self.create_layer(number_hidden)
+        self.output_layer = self.create_layer(number_output)
         self.input_hidden_weights = self.create_weights(number_input, number_hidden, initial_weight_value)
         self.hidden_output_weights = self.create_weights(number_hidden, number_output, initial_weight_value)
+        
     
     def create_weights(self, number_origin_layer, number_target_layer, initial_weight_value):
         weight_matrix = []
-        for i in range(number_origin_layer):
+        for i in range(number_target_layer):
             weights = []
             #BIAS
             weights.append(initial_weight_value)
-            for j in range(number_target_layer):
+            for j in range(number_origin_layer):
                 weights.append(initial_weight_value)
             weight_matrix.append(weights)
         return weight_matrix
             
-    def create_layer(self, layer, number_of_neurons):
+    def create_layer(self, number_of_neurons):
+        result = []
         for i in range(number_of_neurons):
-            layer.append(Neuron())
+            result.append(Neuron())
+        return result            
             
     def propagate_input(self, net_input):
-        print "net input:", net_input
         hidden_output = []
-                
         for i in range(len(self.hidden_layer)):
             # compute input to hidden layer
             # BIAS
             induced_local_field = self.input_hidden_weights[i][0]
-            for j in range(len(net_input)):
-                induced_local_field += net_input[j] * self.input_hidden_weights[j][i+1]
+            for j in range(len(net_input) ):
+                induced_local_field += net_input[j] * self.input_hidden_weights[i][j+1]
             # compute output of hidden layer
-            print "induced_local_field:", induced_local_field
             hidden_output.append(self.hidden_layer[i].compute_output(induced_local_field))
         
-        print "hidden_output:", hidden_output
         output_output = []
         for i in range(len(self.output_layer)):
             #BIAS
             induced_local_field = self.hidden_output_weights[i][0]
             for j in range(len(hidden_output)):
-                induced_local_field += hidden_output[j] * self.hidden_output_weights[j][i+1]
+                induced_local_field += hidden_output[j] * self.hidden_output_weights[i][j+1]
             #output_induced_field.append(induced_local_field)
             output_output.append(self.output_layer[i].compute_output(induced_local_field))
         
@@ -69,8 +68,13 @@ class Neuron:
         
         
 mlp = MLP(2, 2, 1, 0)
-print "input-hidden layer", mlp.input_hidden_weights
-print "hidden-output layer", mlp.hidden_output_weights
+print "input-hidden weights", mlp.input_hidden_weights
+print "hidden-output weights", mlp.hidden_output_weights
 #print mlp.input_hidden_weights
 print mlp.propagate_input([1,1])
+
+mlp2 = MLP(2, 2, 1, 0.5)
+print "input-hidden weights", mlp2.input_hidden_weights
+print "hidden-output weights", mlp2.hidden_output_weights
+print mlp2.propagate_input([1,1])
 
